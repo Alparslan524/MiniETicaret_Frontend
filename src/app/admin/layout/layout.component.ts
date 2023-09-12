28129a244c6fa0d4b3bc4584e5bf8b7ea98be627
Alparslan <alparslanaydgn02@gmail.com>
2023-09-12 14:06:23 +0300

    AlertifyJs kütüphanesi eklendi ve servis yazılarak kendi kullanacağımız şekle göre uyarlandı.

--- src/app/admin/layout/layout.component.ts
+++ src/app/admin/layout/layout.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.scss'],
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit {
+  constructor(private alertify: AlertifyService) { }
 
+  message = "Alertify Çalışıyor!!";
+  
+  ngOnInit(): void {
+    //this.alertify.message(this.message, MessageType.Success, Position.TopRight,1,false);
+    this.alertify.message(this.message, { messageType: MessageType.Success, position: Position.TopRight, delay: 3, dismissOthers:false });
+  }
 }
