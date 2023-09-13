e3022e41382a6a495086e8d1553f89d3c679a68d
Alparslan <alparslanaydgn02@gmail.com>
2023-09-13 16:32:41 +0300

    Toastr kütüphanesi entegere edildi. Kendimize göre modifiye etmek için servis oluşturudlu ve projede kullanacağımız şekilde özelleştirildi.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from './services/ui/custom-toastr.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MiniETicaret';
+  constructor(private toastr: CustomToastrService) {
+    toastr.message("Toastr Çalışıyor!!", "Toastr", { messageType: ToastrMessageType.Error, position: ToastrPosition.TopLeft });
+    toastr.message("Toastr Çalışıyor!!", "Toastr", { messageType: ToastrMessageType.Warning, position: ToastrPosition.TopLeft });
+    toastr.message("Toastr Çalışıyor!!", "Toastr", { messageType: ToastrMessageType.Success, position: ToastrPosition.BottomLeft });
+    toastr.message("Toastr Çalışıyor!!", "Toastr", { messageType: ToastrMessageType.Info, position: ToastrPosition.BottomLeft });
+  }
 }
