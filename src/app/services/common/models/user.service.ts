213ec4c2612c8c38fdeb26d7d609e405e6fc0cec
Alparslan <alparslanaydgn02@gmail.com>
2023-10-31 16:00:51 +0300

    Ders 40 => JWT entegrasyonu yapıldı. Artık giriş yapmadan products dönülmüyor. Giriş yapınca token dönüyor.

--- src/app/services/common/models/user.service.ts
+++ src/app/services/common/models/user.service.ts
@@ -4,12 +4,16 @@ import { User } from 'src/app/entities/User';
 import { CreateUser } from 'src/app/contracts/Users/create_user';
 import { Observable, firstValueFrom } from 'rxjs';
 
+import { AlertifyService, MessageType, Position } from '../../admin/alertify.service';
+import { Token } from 'src/app/contracts/Token/token';
+import { TokenResponse } from 'src/app/contracts/Token/tokenResponse';
+
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private httpClientService: HttpClientService) { }
+  constructor(private httpClientService: HttpClientService, private alertifyService: AlertifyService) { }
 
   async create(user: User) {
     const observable: Observable<CreateUser | User> = this.httpClientService.post<CreateUser | User>({
@@ -18,12 +22,21 @@ export class UserService {
     return await firstValueFrom(observable) as CreateUser;
   }
 
-  async login(userNameorEmail: string, Password: string, callBack?: () => void): Promise<void> {
-    const observable: Observable<any> = this.httpClientService.post({
+  async login(userNameorEmail: string, Password: string, callBack?: () => void): Promise<any> {
+    const observable: Observable<any | TokenResponse> = this.httpClientService.post<any | TokenResponse>({
       controller: "users",
       action: "login"
     }, { userNameorEmail, Password })
-    await firstValueFrom(observable);
+    const tokenResponse: TokenResponse = await firstValueFrom(observable) as TokenResponse;
+    if (tokenResponse) {
+
+      localStorage.setItem("accessToken", tokenResponse.token.accessToken);
+
+      this.alertifyService.message("Kullanıcı girişi başarılı", {
+        messageType: MessageType.Success,
+        position: Position.TopRight
+      })
+    }
     callBack();
   }
 }
